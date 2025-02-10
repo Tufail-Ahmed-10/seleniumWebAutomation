62cd053a0c104a08d9e058cd9142c59bd27dd4ac
Tufail-Ahmed-10 <tufailahmed2018331010@gmail.com>
2025-02-10 21:10:16 +0600

    first commit

---
+++ src/test/java/seleniumclass1/LaunchBrowser.java
@@ -0,0 +1,13 @@
+package seleniumclass1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        WebDriver driver =new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://mvnrepository.com/");
+        driver.quit();
+    }
+}
