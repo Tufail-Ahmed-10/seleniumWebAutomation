d7e9dde1be09795b557f96504239bc14409bd4d2
Tufail-Ahmed-10 <tufailahmed2018331010@gmail.com>
2025-02-11 13:44:11 +0600

    LoginwithIdNameTest LoginwithIdSelectorTest

---
+++ src/test/java/seleniumclass1/LoginwithIdSelectorTest.java
@@ -0,0 +1,19 @@
+package seleniumclass1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginwithIdSelectorTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver= new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("login-button")).click();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
