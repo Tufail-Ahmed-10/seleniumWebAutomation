f946bb6dc5abd0bfc3f45d0a4af8682b89cf15f7
Tufail-Ahmed-10 <tufailahmed2018331010@gmail.com>
2025-02-11 12:27:21 +0600

    selenium 1st class:Login

--- src/test/java/seleniumclass1/LaunchBrowser.java
+++ src/test/java/seleniumclass1/LaunchBrowser.java
@@ -7,7 +7,7 @@ public class LaunchBrowser {
     public static void main(String[] args) {
         WebDriver driver =new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://mvnrepository.com/");
+        System.out.println(driver.getCurrentUrl());
         driver.quit();
     }
 }
